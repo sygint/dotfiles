2672b3322eaf7f7d28bb0b78483855ed6476245a
installer <installer@gmail.com>
2024-12-12 01:58:38 -0800

    Add btop and kitty to home packages

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -16,11 +16,35 @@ in
   # Install & Configure Git
   programs = {
     home-manager.enable = true;
-    
+
+    btop = {
+      enable = true;
+      settings = {
+        vim_keys = true;
+      };
+    };
+
     git = {
       enable = true;
       userName = "${gitUsername}";
       userEmail = "${gitEmail}";
+    };
+
+    kitty = {
+      enable = true;
+      package = pkgs.kitty;
+      settings = {
+        scrollback_lines = 2000;
+        wheel_scroll_min_lines = 1;
+        window_padding_width = 4;
+        confirm_os_window_close = 0;
+      };
+      extraConfig = ''
+        tab_bar_style fade
+        tab_fade 1
+        active_tab_font_style   bold
+        inactive_tab_font_style bold
+      '';
     };
   };
 
