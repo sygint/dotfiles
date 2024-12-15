2ae63d25c4da813d351af58a6348a25186eb8b95
syg <sygint@users.noreply.github.com>
2024-12-15 01:51:37 -0800

    Migrate kitty config as nix

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -43,18 +43,7 @@ in
     kitty = {
       enable = true;
       package = pkgs.kitty;
-      settings = {
-        scrollback_lines = 2000;
-        wheel_scroll_min_lines = 1;
-        window_padding_width = 4;
-        confirm_os_window_close = 0;
-      };
-      extraConfig = ''
-        tab_bar_style fade
-        tab_fade 1
-        active_tab_font_style   bold
-        inactive_tab_font_style bold
-      '';
+      extraConfig =  (import ../../config/kitty/kitty.nix);
     };
   };
 
