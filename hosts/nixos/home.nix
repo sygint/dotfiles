ffa1b9c537e89bafd2d0d675bdbfc487f29c2d91
syg <sygint@users.noreply.github.com>
2024-12-17 23:10:13 -0800

    Update hypridle timeouts

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -69,19 +69,19 @@ in
         listener = [
           # Screenlock
           {
-            timeout    = 60;
+            timeout    = 300;
             on-timeout = "hyprlock";
           }
           # DPMS
           {
-            timeout    = 300;
+            timeout    = 600;
             on-timeout = "hyprctl dispatch dpms off";
             on-resume  = "hyprctl dispatch dpms on";
           }
           # Suspend
           {
-              timeout    = 1200;
-              on-timeout = "systemctl suspend";
+            timeout    = 1200;
+            on-timeout = "systemctl suspend";
           }
         ];
       };
