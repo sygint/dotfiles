e9782a19d0c71a9232b1825000090b12def26b0a
Sygint <sygint@users.noreply.github.com>
2025-03-29 01:38:51 -0700

    Move hypridle to Hyprland module

--- modules/home/programs/hypr/hyprland.nix
+++ modules/home/programs/hypr/hyprland.nix
@@ -31,35 +31,40 @@ in {
       extraConfig = (import ../../../../config/hypr/hyprlock.conf.nix { inherit username; });
     };
 
-    # services.hypridle = {
-    #   enable   = true;
-    #   settings = {
-    #     general = {
-    #       lock_cmd            = "pidof hyprlock || hyprlock";  # avoid starting multiple hyprlock instances.
-    #       before_sleep_cmd    = "loginctl lock-session";       # lock before suspend.
-    #       after_sleep_cmd     = "hyprctl dispatch dpms on";    # to avoid having to press a key twice to turn on the display.
-    #       ignore_dbus_inhibit = false;
-    #     };
+    services = {
+      hypridle = {
+        enable   = true;
+        settings = {
+          general = {
+            lock_cmd            = "pidof hyprlock || hyprlock";  # avoid starting multiple hyprlock instances.
+            before_sleep_cmd    = "loginctl lock-session";       # lock before suspend.
+            after_sleep_cmd     = "hyprctl dispatch dpms on";    # to avoid having to press a key twice to turn on the display.
+            ignore_dbus_inhibit = false;
+          };
 
-    #     listener = [
-    #       # Screenlock
-    #       {
-    #         timeout    = 300;
-    #         on-timeout = "hyprlock";
-    #       }
-    #       # DPMS
-    #       {
-    #         timeout    = 600;
-    #         on-timeout = "hyprctl dispatch dpms off";
-    #         on-resume  = "hyprctl dispatch dpms on";
-    #       }
-    #       # Suspend
-    #       # {
-    #       #   timeout    = 300;
-    #       #   on-timeout = "systemctl suspend";
-    #       # }
-    #     ];
-    #   };
-    # };
+          listener = [
+            # Screenlock
+            {
+              timeout    = 300;
+              on-timeout = "hyprlock";
+            }
+            # DPMS
+            {
+              timeout    = 600;
+              on-timeout = "hyprctl dispatch dpms off";
+              on-resume  = "hyprctl dispatch dpms on";
+            }
+            # Suspend
+            {
+              timeout    = 1200;
+              on-timeout = "systemctl suspend";
+            }
+          ];
+        };
+      };
+
+      # This doesn't seem to work for some reason
+      # xdg-desktop-portal-hyprland.enable = true;
+    };
   };
 }
