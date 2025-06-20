5328b4f1420dae2ef0d40eeaaba4e939abe0283c
Sygint <sygint@users.noreply.github.com>
2025-06-19 20:14:47 -0700

    feat: separate Nix and template variables for better organization
    - Add chezmoi.toml for application template variables
    - Clean up variables.nix to only include NixOS system variables
    - Add cross-reference comments between variable files
    - Improve variable organization and maintainability

--- variables.nix
+++ variables.nix
@@ -1,17 +1,13 @@
 {
+  # NixOS system configuration variables
+  # Note: Application preferences (terminal, browser, etc.) are in dotfiles/chezmoi.toml
+  
   hostName = "nixos";
   username = "syg";
 
   # Git Configuration ( For Pulling Software Repos )
   gitUsername = "Sygint";
   gitEmail    = "sygint@users.noreply.github.com";
-
-  # Program options
-  fileBrowser    = "nemo";
-  webBrowser     = "brave";
-  terminal       = "ghostty";
-  keyboardLayout = "us";
-  menu           = "rofi -show drun";
 
   # Password for Syncthing
   syncPassword = "syncmybattleship";
