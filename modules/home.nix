a9d9fcca9ee861bde8676a2e61480289620b3597
sygint <sygint@users.noreply.github.com>
2025-11-21 00:07:27 -0800

    refactor(home.nix): streamline program imports using file filtering

--- modules/home.nix
+++ modules/home.nix
@@ -1,23 +1,14 @@
+{ lib, ... }:
 {
-  imports = [
-    # Program modules
-    ./home/programs/git.nix
-    ./home/programs/brave.nix
-    ./home/programs/librewolf.nix
-    ./home/programs/vscode.nix
-    ./home/programs/btop.nix
-    ./home/programs/kitty.nix
-    ./home/programs/zsh.nix
-    ./home/programs/hyprland.nix
-    ./home/programs/hypridle.nix
-    ./home/programs/hyprpanel.nix
-    ./home/programs/waybar.nix
-    ./home/programs/devenv.nix
-    ./home/programs/protonmail-bridge.nix
-    ./home/programs/mullvad.nix
-    ./home/programs/screenshots.nix
-    ./home/programs/archiver.nix
-  ];
+  imports = lib.fileset.toList (
+    lib.fileset.fileFilter
+      (file:
+        file.hasExt "nix"
+        && file.name != "default.nix"
+        && !lib.strings.hasSuffix ".conf.nix" file.name
+      )
+      ./home/programs
+  );
 
   # Home Manager state version (should match system.stateVersion)
   home.stateVersion = "24.11";
