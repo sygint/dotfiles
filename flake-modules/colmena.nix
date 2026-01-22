ae5a2d8d1e2f6e4b3a1b8d498def94af034764e8
sygint <sygint@users.noreply.github.com>
2026-01-22 10:50:14 -0800

    fix: avoid infinite recursion in colmena hasSecrets

--- flake-modules/colmena.nix
+++ flake-modules/colmena.nix
@@ -89,10 +89,14 @@ in
       };
 
       # Import the same modules as nixosConfigurations
-      imports = [ systemCfg.path ] ++ systemCfg.modules;
-
-      # Pass hasSecrets to the configuration
-      _module.args.hasSecrets = systemCfg.hasSecrets;
+      imports = [
+        systemCfg.path
+      ]
+      ++ systemCfg.modules
+      ++ [
+        # Pass hasSecrets as a module option to avoid infinite recursion
+        { config._module.args.hasSecrets = systemCfg.hasSecrets; }
+      ];
     }
   ) fleetConfig.hosts;
 }
