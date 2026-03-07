162525f34c8f8173b00cad2506c3bfa23089506e
sygint <sygint@users.noreply.github.com>
2026-03-06 17:51:17 -0800

    feat: add uBlock Origin extension to Firefox via policies

--- modules/features/firefox.nix
+++ modules/features/firefox.nix
@@ -21,6 +21,23 @@ in
       programs.firefox = {
         enable = true;
 
+        policies = {
+          ExtensionSettings =
+            with builtins;
+            let
+              extension = shortId: uuid: {
+                name = uuid;
+                value = {
+                  install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+                  installation_mode = "normal_installed";
+                };
+              };
+            in
+            listToAttrs [
+              (extension "ublock-origin" "uBlock0@raymondhill.net")
+            ];
+        };
+
         profiles.default = {
           name = "Default";
           isDefault = true;
